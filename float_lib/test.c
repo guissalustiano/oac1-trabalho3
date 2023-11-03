6051d0228b2b12dabe28d51a1804f8ac30269f51
Guilherme S. Salustiano <guissalustiano@gmail.com>
2023-11-03 11:41:42 +0100

    add hard f2i and i2f tests

--- float_lib/test.c
+++ float_lib/test.c
@@ -1,35 +1,89 @@
 #include "float_lib.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <float.h>
 
-void assert_mint(mint a, mint b) {
+# define true 1
+# define false 0
+
+#define ACCEPT_ERROR 0.00001
+
+// from https://math.stackexchange.com/a/4508789
+uint8_t within(float a, float b, float tol) {
+    float d = fabs(a - b);
+    if (d == 0) {
+        return true;
+    }
+    // Since d =/= 0 we know a, b =/= 0 so division by zero will not happen
+    return d/(fabs(a)+fabs(b)) < tol;
+}
+
+typedef union {
+    mfloat i; // acess as int
+    float  f; // acess as float
+              // Lembrando que na implementacao de voces, voces nao podem usar float!
+} floatint;
+
+void assert_mint(int32_t a, int32_t b) {
     if (a != b) {
-        printf("Expected %d, got %d\n", b, a);
-        exit(1);
+        printf("ERROR. Expected %d, got %d\n", b, a);
+    } else {
+        printf("OK. %d\n", a);
     }
 }
 
-void assert_mfloat(mfloat a, mfloat b) {
-    if (a != b) {
-        printf("Expected %x, got %x\n", b, a);
-        exit(1);
+void assert_float(floatint a, floatint b) {
+    if (!within(a.f, b.f, ACCEPT_ERROR)) {
+        printf("ERROR. Expected %.3f (%x), got %.3f (%x)\n", b.f, b.i, a.f, a.i);
+    } else {
+        printf("OK. %.3f (%x)\n", a.f, a.i);
     }
 }
 
-// You can use https://www.h-schmidt.net/FloatConverter/IEEE754.html
+void assert_floatsisf(int32_t a) {
+    floatint actual, expected;
+
+    actual.i = floatsisf(a);
+    expected.f = (float) a;
+
+    assert_float(actual, expected);
+}
+
+// Nao sera testado o caso de 0, pois zero em float point eh um caso especial
+void test_cases_floatsist() {
+    for (mint i = INT32_MIN; i < -0; i/=10) {
+        assert_floatsisf(i);
+    }
+    for (mint i = INT32_MAX; i > 0; i/=10) {
+        assert_floatsisf(i);
+    }
+}
+
+void assert_fixsfsi(float a) {
+    floatint arg;
+    mint actual, expected;
+
+    arg.f = a;
+    actual = fixsfsi(arg.i);
+    expected = (mint) a;
+
+    assert_mint(actual, expected);
+}
+
+void test_cases_fixsfsi() {
+    for (float i = INT32_MIN; i < -0; i/=10) {
+        assert_fixsfsi(i);
+    }
+    for (float i = INT32_MAX; i > 0; i/=10) {
+        assert_fixsfsi(i);
+    }
+}
+
 int main() {
-    mint a_int = 10;
-    mint b_int = -20;
-
-    mfloat a_float = floatsisf(a_int);
-    mfloat b_float = floatsisf(b_int);
-
-    assert_mfloat(a_float, 0x41200000);
-    assert_mfloat(b_float, 0xc1a00000);
-
-    mfloat res_float = subsf3(a_float, b_float);
-    mint res_int = fixsfsi(res_float);
-
-    assert_mint(res_int, 30);
+    test_cases_floatsist();
+    test_cases_fixsfsi();
+    return 0;
 }
 
